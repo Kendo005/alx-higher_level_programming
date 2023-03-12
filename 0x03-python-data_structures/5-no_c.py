debe22c158c31aa9efe8e8ba8ad8fd3ec605b9ba
kendo005 <kennzhood@yahoo.com>
2023-03-12 07:44:08 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
-# return my_string.translate({ord(c): None for c in "cC"})
 
+
+def no_c(my_string):
+    """
+    removes all instances of 'c' & 'C' from string
+    """
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_str += my_string[i]
+    return (new_str)
