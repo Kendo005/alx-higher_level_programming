03698c88f15b4444c86e9582a116d261a14eb01e
kendo005 <kennzhood@yahoo.com>
2023-04-21 05:48:13 -0700

    more commits

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Module containing the function read_file"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """Reads a file and prints to stdout.
-    Args:
-        filename (str, optional): name of file to read. Defaults to "".
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
